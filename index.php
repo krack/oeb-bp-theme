7bc6f407a171c23cc528c3093aaffffd08e49a3d
Sylvain Gandon <sylvain.gandon@soprasteria.com>
2018-08-28 13:03:21 +0200

    base of theme

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php wp_head(); ?>
+</head>
+
+<body>
+	<div id="page" class="site">
+		<header id="masthead" class="site-header" role="banner">
+			<h1>Ongles et beauté</h1>
+			<h2>Prochainement, votre nouvel institut de beauté à Sablé sur Sarthe</h2>
+		</header><!-- #masthead -->
+
+	</div>
+</body>
+</html>
